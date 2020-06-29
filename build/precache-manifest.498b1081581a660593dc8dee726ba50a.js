6673ab9a234af2b17b5f2b1e6c173a4fa56e29fc
Amado Guerrero <amado4884@gmail.com>
2020-06-28 22:07:22 -0400

    built

---
+++ build/precache-manifest.498b1081581a660593dc8dee726ba50a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1782fec2c4ad1a6ecaf39f7b61b687cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0e0a032db141fad4b0",
+    "url": "/static/css/main.20894453.chunk.css"
+  },
+  {
+    "revision": "bcbc530582294987a9db",
+    "url": "/static/js/2.2eb8e125.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2eb8e125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0e0a032db141fad4b0",
+    "url": "/static/js/main.cf41310b.chunk.js"
+  },
+  {
+    "revision": "4248628d849dc20abedc",
+    "url": "/static/js/runtime-main.cd18bb95.js"
+  },
+  {
+    "revision": "8fb7702c55568cb5513d01538ccb2682",
+    "url": "/static/media/pk-bg.8fb7702c.jpg"
+  }
+]);
